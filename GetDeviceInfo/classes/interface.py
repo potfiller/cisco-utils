fa7ebffd50b908792c5d2580b39734c16390fedc
Ben Philpot <bmphilpot@gmail.com>
2021-05-13 17:19:24 +0100

    includes vlan and port mode information

--- GetDeviceInfo/classes/interface.py
+++ GetDeviceInfo/classes/interface.py
@@ -1,14 +1,6 @@
 """The Interface class"""
 
-from enum import Enum
 from dataclasses import dataclass
-
-class IntType(Enum):
-    """All ports are one of these"""
-
-    ACCESS = 1
-    TRUNK = 2
-    ROUTED = 3
 
 @dataclass
 class Interface:
@@ -16,6 +8,32 @@ class Interface:
 
     name: str
     l2_addr: str
-    l3_addr: str
-    int_type: IntType
-    enabled: bool
+    l3_addr: list
+    vlans: list
+    mode: str
+    is_enabled: bool
+    is_up: bool
+
+
+    def l3_addr_str(self) -> str:
+        """Return the L3 address as str list"""
+
+        addr_str = ""
+        if len(self.l3_addr) > 0:
+            for address in self.l3_addr:
+                addr_str += f"{address}\n"
+            strend = len(addr_str)-1
+            addr_str = addr_str[0: strend]
+        return addr_str
+
+
+    def vlans_str(self) -> str:
+        """Return the vlans as str list"""
+
+        vlans_str = ""
+        if len(self.vlans) > 0:
+            for vlan in self.vlans:
+                vlans_str += f"{vlan}\n"
+            strend = len(vlans_str)-1
+            vlans_str = vlans_str[0: strend]
+        return vlans_str
--- GetDeviceInfo/main.py
+++ GetDeviceInfo/main.py
@@ -4,14 +4,19 @@ using nornir and napalm
 
 import os
 from datetime import datetime
-from nornir import InitNornir
-from nornir_napalm.plugins.tasks import napalm_get
+
 import yaml
+from nornir import InitNornir
+from nornir.core.task import Task, Result
+from nornir_napalm.plugins.tasks import napalm_get, napalm_cli
 from prettytable import PrettyTable
+from ntc_templates.parse import parse_output
+
 from classes.device import Device
-from classes.interface import Interface, IntType
+from classes.interface import Interface
 
 REPORTDIR = "reports\\"
+
 
 def write_file(filecontent: str):
     """Write the obtained information to a file
@@ -20,77 +25,142 @@ def write_file(filecontent: str):
     File suffix is txt.
     """
 
-    #create reports folder
+    # create reports folder
     if not os.path.exists(REPORTDIR):
         os.mkdir(REPORTDIR)
 
-    #prepare filename
+    # prepare filename
     datestr = datetime.today().strftime("%Y-%m-%d_%H-%M-%S")
     filename = f"inventory_{datestr}.txt"
 
-    #write file
+    # write file
     with open(os.path.join(REPORTDIR, filename), "w") as file_to_write:
         file_to_write.write(filecontent)
-
-
-def create_device_table(devices: list) -> PrettyTable:
-    """Create a PrettyTable from the list of devices"""
-
-    table = PrettyTable()
-    table.field_names = ['hostname','model','os_version']
-    for device in devices:
-        table.add_row([device.hostname, device.model, device.os_version])
-    return table
 
 
 def create_interface_table(device_name: str, interfaces: list) -> PrettyTable:
     """Create a PrettyTable from the list of device interfaces"""
 
     table = PrettyTable()
-    table.field_names = [device_name]
+    table.title = device_name
+    table.field_names = ['Interface', 'L2 Addr', 'vlans', 'L3 Addr', 'Mode', 'Enabled', 'Up']
     for interface in interfaces:
-        table.add_row([interface.name])
+        table.add_row([
+            interface.name,
+            interface.l2_addr,
+            interface.vlans_str(),
+            interface.l3_addr_str(),
+            interface.mode,
+            interface.is_enabled,
+            interface.is_up])
+    return table
+
+
+def create_device_table(devices: list) -> PrettyTable:
+    """Create a PrettyTable from the list of devices"""
+
+    table = PrettyTable()
+    table.field_names = ['hostname', 'model', 'os_version']
+    for device in devices:
+        table.add_row([device.hostname, device.model, device.os_version])
     return table
 
 
 def create_model(result) -> list:
-    """Create a model from the nornir results"""
+    """Create a list of devices from the nornir results"""
 
-    devices = []
+    devices = []    # list[Device]
     for host in result.keys():
         if not host in result.failed_hosts.keys():
             device = Device(
-                hostname = result[host][0].result['get_facts']['hostname'],
-                model = result[host][0].result['get_facts']['model'],
-                os_version = result[host][0].result['get_facts']['os_version'],
-                interfaces = []
+                hostname=result[host][1].result['get_facts']['hostname'],
+                model=result[host][1].result['get_facts']['model'],
+                os_version=result[host][1].result['get_facts']['os_version'],
+                interfaces=[]
             )
-            for int_name in result[host][0].result['get_facts']['interface_list']:
-                device.interfaces.append(
-                    Interface(
-                        name = int_name,
-                        l2_addr = "",
-                        l3_addr = "",
-                        int_type = IntType.ACCESS,
-                        enabled = False
-                    )
+            for int_name in result[host][1].result['get_interfaces']:
+
+                ### get_interfaces ###
+                interface = result[host][1].result['get_interfaces'][int_name]
+                new_interface = Interface(
+                    name=int_name,
+                    l2_addr=interface['mac_address'],
+                    vlans = [],
+                    l3_addr=[],
+                    mode = "",
+                    is_enabled=interface['is_enabled'],
+                    is_up=interface['is_up']
                 )
+                #does this interface have an IP record?
+                if int_name in result[host][1].result['get_interfaces_ip']:
+                    new_interface.mode = "routed"
+
+                    ### get_interfaces_ip ###
+                    int_ip = result[host][1].result['get_interfaces_ip'][int_name]['ipv4']
+                    for ip_address in int_ip.keys():
+                        int_ip_address = ip_address
+                        int_ip_prefix = int_ip[ip_address]['prefix_length']
+                        new_interface.l3_addr.append(f"{int_ip_address}/{int_ip_prefix}")
+
+                ### get_vlans ###
+                vlan_list = result[host][1].result['get_vlans']
+                for vlanid in vlan_list.keys():
+                    if int_name in vlan_list[vlanid]['interfaces']:
+                        new_interface.vlans.append(vlanid)
+
+                ### show interfaces switchport ###
+                output_str = result[host][2].result['show interfaces switchport']
+                output_parsed = parse_output(
+                                    platform="cisco_ios",
+                                    command="show interfaces switchport",
+                                    data=output_str)
+                for entry in output_parsed:
+                    #entry['interface'] will be Fa0/1 or similar
+                    ent_int = entry['interface']
+                    fa_gi = ent_int[0:2]    # Fa
+                    int_id = ent_int[2-len(ent_int):]   # 0/1
+                    int_mode = entry['mode']
+                    if (fa_gi.lower() in int_name.lower()) and (int_id in int_name):
+                        new_interface.mode = int_mode
+
+                device.interfaces.append(new_interface)
             devices.append(device)
     return devices
+
+
+def master_task(task: Task) -> Result:
+    """Runs the napalm tasks"""
+
+    task.run(
+        task=napalm_get,
+        getters=["get_facts", "get_interfaces", "get_interfaces_ip", "get_vlans"]
+    )
+    task.run(
+        task=napalm_cli,
+        commands=["show interfaces switchport"]
+    )
+    return Result(
+        host=task.host,
+        result=f"{task.host} finished."
+    )
 
 
 def nornir_code(creds: dict):
     """Execute the nornir tasks and process results"""
 
-    #update and filter inventory
+    # update and filter inventory
     nornir_obj = InitNornir(config_file="config.yaml")
     nornir_obj.inventory.groups["cisco_group"].username = creds['username']
     nornir_obj.inventory.groups["cisco_group"].password = creds['password']
 
-    #start main task
+    # # start main task
+    # result = nornir_obj.run(
+    #     task=napalm_get,
+    #     getters=["get_facts", "get_interfaces", "get_interfaces_ip", "get_vlans"]
+    # )
+
     result = nornir_obj.run(
-        task=napalm_get,
-        getters=["get_facts"]
+        task=master_task
     )
 
     devices = create_model(result)
@@ -109,13 +179,14 @@ def nornir_code(creds: dict):
 def setup_creds() -> dict:
     """retrieve credentials"""
 
-    #get credentials
+    # get credentials
     creds = yaml.safe_load(open('C:\\Users\\Ben\\python\\creds.yaml'))
 
     return {
         'username': creds['user']['username'],
         'password': creds['user']['password']
     }
+
 
 def main():
     """app entry point"""
