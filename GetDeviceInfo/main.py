a2663b20f1582f9430bf90029c0827d240e12a15
Ben Philpot <bmphilpot@gmail.com>
2021-05-14 22:29:26 +0100

    Refactored.

--- GetDeviceInfo/main.py
+++ GetDeviceInfo/main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 import yaml
 from nornir import InitNornir
-from nornir.core.task import Task, Result
+from nornir.core.task import AggregatedResult, Task, Result
 from nornir_napalm.plugins.tasks import napalm_get, napalm_cli
 from prettytable import PrettyTable
 from ntc_templates.parse import parse_output
@@ -43,7 +43,8 @@ def create_interface_table(device_name: str, interfaces: list) -> PrettyTable:
 
     table = PrettyTable()
     table.title = device_name
-    table.field_names = ['Interface', 'L2 Addr', 'vlans', 'L3 Addr', 'Mode', 'Enabled', 'Up']
+    table.field_names = ['Interface', 'L2 Addr',
+                         'vlans', 'L3 Addr', 'Mode', 'Enabled', 'Up']
     for interface in interfaces:
         table.add_row([
             interface.name,
@@ -66,7 +67,81 @@ def create_device_table(devices: list) -> PrettyTable:
     return table
 
 
-def create_model(result) -> list:
+def get_int_mode(host: str, int_name: str, result: AggregatedResult) -> str:
+    """Get the mode(static, trunk, down) for an interface"""
+
+    mode = ""
+    ### show interfaces switchport ###
+    output_parsed = parse_output(
+        platform="cisco_ios",
+        command="show interfaces switchport",
+        data=result[host][2].result['show interfaces switchport']
+    )
+    for entry in output_parsed:
+        # entry['interface'] will be Fa0/1 or similar
+        ent_int = entry['interface']
+        fa_gi = ent_int[0:2]    # Fa
+        int_id = ent_int[2-len(ent_int):]   # 0/1
+        int_mode = entry['mode']
+        if (fa_gi.lower() in int_name.lower()) and (int_id in int_name):
+            mode = int_mode
+
+    return mode
+
+
+def build_vlans(host: str, int_name: str,  result: AggregatedResult) -> list:
+    """Build a list of vlans for an interface and host"""
+
+    vlan_ids = []
+    vlan_list = result[host][1].result['get_vlans']
+    for vlanid in vlan_list.keys():
+        if int_name in vlan_list[vlanid]['interfaces']:
+            vlan_ids.append(vlanid)
+
+    return vlan_ids
+
+
+def build_interfaces(host: str, result: AggregatedResult) -> list:
+    """Build a list of interfaces for the host"""
+
+    interfaces = []  # list[Interface]
+    for int_name in result[host][1].result['get_interfaces']:
+
+        ### get_interfaces ###
+        interface = result[host][1].result['get_interfaces'][int_name]
+        new_interface = Interface(
+            name=int_name,
+            l2_addr=interface['mac_address'],
+            vlans=[],
+            l3_addr=[],
+            mode="",
+            is_enabled=interface['is_enabled'],
+            is_up=interface['is_up']
+        )
+        # does this interface have an IP record?
+        if int_name in result[host][1].result['get_interfaces_ip']:
+            new_interface.mode = "routed"
+
+            ### get_interfaces_ip ###
+            int_ip = result[host][1].result['get_interfaces_ip'][int_name]['ipv4']
+            for ip_address in int_ip.keys():
+                int_ip_address = ip_address
+                int_ip_prefix = int_ip[ip_address]['prefix_length']
+                new_interface.l3_addr.append(
+                    f"{int_ip_address}/{int_ip_prefix}")
+
+        new_interface.vlans = build_vlans(host, int_name, result)
+
+        #check the mode for L2 ports
+        if new_interface.l3_addr_str() == "":
+            new_interface.mode = get_int_mode(host, int_name, result)
+
+        interfaces.append(new_interface)
+
+    return interfaces
+
+
+def create_model(result: AggregatedResult) -> list:
     """Create a list of devices from the nornir results"""
 
     devices = []    # list[Device]
@@ -78,53 +153,9 @@ def create_model(result) -> list:
                 os_version=result[host][1].result['get_facts']['os_version'],
                 interfaces=[]
             )
-            for int_name in result[host][1].result['get_interfaces']:
-
-                ### get_interfaces ###
-                interface = result[host][1].result['get_interfaces'][int_name]
-                new_interface = Interface(
-                    name=int_name,
-                    l2_addr=interface['mac_address'],
-                    vlans = [],
-                    l3_addr=[],
-                    mode = "",
-                    is_enabled=interface['is_enabled'],
-                    is_up=interface['is_up']
-                )
-                #does this interface have an IP record?
-                if int_name in result[host][1].result['get_interfaces_ip']:
-                    new_interface.mode = "routed"
-
-                    ### get_interfaces_ip ###
-                    int_ip = result[host][1].result['get_interfaces_ip'][int_name]['ipv4']
-                    for ip_address in int_ip.keys():
-                        int_ip_address = ip_address
-                        int_ip_prefix = int_ip[ip_address]['prefix_length']
-                        new_interface.l3_addr.append(f"{int_ip_address}/{int_ip_prefix}")
-
-                ### get_vlans ###
-                vlan_list = result[host][1].result['get_vlans']
-                for vlanid in vlan_list.keys():
-                    if int_name in vlan_list[vlanid]['interfaces']:
-                        new_interface.vlans.append(vlanid)
-
-                ### show interfaces switchport ###
-                output_str = result[host][2].result['show interfaces switchport']
-                output_parsed = parse_output(
-                                    platform="cisco_ios",
-                                    command="show interfaces switchport",
-                                    data=output_str)
-                for entry in output_parsed:
-                    #entry['interface'] will be Fa0/1 or similar
-                    ent_int = entry['interface']
-                    fa_gi = ent_int[0:2]    # Fa
-                    int_id = ent_int[2-len(ent_int):]   # 0/1
-                    int_mode = entry['mode']
-                    if (fa_gi.lower() in int_name.lower()) and (int_id in int_name):
-                        new_interface.mode = int_mode
-
-                device.interfaces.append(new_interface)
+            device.interfaces = build_interfaces(host, result)
             devices.append(device)
+
     return devices
 
 
@@ -133,7 +164,8 @@ def master_task(task: Task) -> Result:
 
     task.run(
         task=napalm_get,
-        getters=["get_facts", "get_interfaces", "get_interfaces_ip", "get_vlans"]
+        getters=["get_facts", "get_interfaces",
+                 "get_interfaces_ip", "get_vlans"]
     )
     task.run(
         task=napalm_cli,
@@ -145,19 +177,27 @@ def master_task(task: Task) -> Result:
     )
 
 
-def nornir_code(creds: dict):
+def setup_creds() -> dict:
+    """retrieve credentials"""
+
+    # get credentials
+    creds = yaml.safe_load(open('C:\\Users\\Ben\\python\\creds.yaml'))
+
+    return {
+        'username': creds['user']['username'],
+        'password': creds['user']['password']
+    }
+
+
+def main():
     """Execute the nornir tasks and process results"""
+
+    creds = setup_creds()
 
     # update and filter inventory
     nornir_obj = InitNornir(config_file="config.yaml")
     nornir_obj.inventory.groups["cisco_group"].username = creds['username']
     nornir_obj.inventory.groups["cisco_group"].password = creds['password']
-
-    # # start main task
-    # result = nornir_obj.run(
-    #     task=napalm_get,
-    #     getters=["get_facts", "get_interfaces", "get_interfaces_ip", "get_vlans"]
-    # )
 
     result = nornir_obj.run(
         task=master_task
@@ -174,25 +214,6 @@ def nornir_code(creds: dict):
         print(int_table)
 
     write_file(device_content)
-
-
-def setup_creds() -> dict:
-    """retrieve credentials"""
-
-    # get credentials
-    creds = yaml.safe_load(open('C:\\Users\\Ben\\python\\creds.yaml'))
-
-    return {
-        'username': creds['user']['username'],
-        'password': creds['user']['password']
-    }
-
-
-def main():
-    """app entry point"""
-
-    creds = setup_creds()
-    nornir_code(creds)
 
 
 if __name__ == "__main__":
